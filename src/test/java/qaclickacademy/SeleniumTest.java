841f3460c329464afaa3655900330991402763ed
Balraj <balrajmahar07@gmail.com>
2021-05-27 20:44:07 +0530

    First commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void sleniumTest()
+	{
+		System.out.println("Selenium");
+		
+	}
+	@Test
+	public void uiInternal()
+	{
+		System.out.println("uiInternal");
+	}
+}
+
+
+
