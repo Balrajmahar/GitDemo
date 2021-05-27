c8e2d0e606968692a12800b0848b7f02c76b13fe
balraj <balrajmahar07@gmail.com>
2021-05-27 23:36:00 +0530

    this is from legion

--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -1,5 +1,6 @@
 package qaclickacademy;
 
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
@@ -7,14 +8,21 @@ public class SeleniumTest {
 	public void sleniumTest()
 	{
 		System.out.println("Selenium");
-		
+		Reporter.log("it is updated one",true);
+		Reporter.log("it is updated two",true);
+		Reporter.log("it is updated three",true);
 	}
 	@Test
 	public void uiInternal()
 	{
 		System.out.println("uiInternal");
 	}
+	@Test
+	public void getVideo() {
+		Reporter.log("yeah this is new method", true);
+	}
 }
+
 
 
 
